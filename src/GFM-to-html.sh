e66013e2954ddf49465e70bb3a39baf4bcc86acd
Arthur Lu <learthurgo@gmail.com>
2021-12-29 00:07:17 +0000

    populated .gitignore, added Makefile, added auto-portfolio.c, added file redirection and usage checking to GFM-to-html.sh

--- src/GFM-to-html.sh
+++ src/GFM-to-html.sh
@@ -1,7 +1,12 @@
+if [ $# != 2 ]
+then
+	echo "Usage: GFM-to-html <markdown file> <html file>"
+	exit 2
+fi
 data="$(
 	cat "$1" \
 		| sed 's/"/\\"/g' \
 		| sed ':a;N;$!ba;s/\n/\\n/g' \
 )"
 
-echo $(curl -s --data "{\"text\":\"$data\",\"mode\":\"gfm\"}" 'https://api.github.com/markdown')
+echo -ne $(curl -s --data "{\"text\":\"$data\",\"mode\":\"gfm\"}" 'https://api.github.com/markdown') > $2
