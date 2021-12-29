9bc57e6cc01cf1675ec9663b1aee3a957f2216c5
Arthur Lu <learthurgo@gmail.com>
2021-12-29 19:40:28 +0000

    finished run script

--- src/run-auto-portfolio.sh
+++ src/run-auto-portfolio.sh
@@ -14,12 +14,14 @@ do
 	readarray -d "," -t array <<<"$line"
 	link="$(echo -n ${array[0]})"
 	name="$(echo -n ${array[1]}).markdown"
+	template_target="$(echo -n ${array[1]}).template"
+	template_actual="$(echo -n ${array[2]})"
 	wget $link -O $name &> /dev/null
 	./GFM-to-html.sh $name $name
+	cp $template_actual $template_target
 	names+=("$(echo -n ${array[1]})")
 done
 
-for name in $names
-do
-	echo $name
-done
+./auto-portfolio $(echo ${names[@]})
+rm -f *.markdown
+rm -f *.template
